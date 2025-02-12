931bc76823d0ef0855e019ed473c9029e8bfb1c4
unknown <kapilnamdeo2003@gmail>
2025-02-12 12:05:14 +0530

    [Kapil Namdeo]add. Subset Check program

---
+++ day2/SetInterface/SubsetCheck.java
@@ -0,0 +1,24 @@
+package com.capgeminiTrainingPrograms.week4.day2.SetInterface;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SubsetCheck {
+    public static boolean isSubset(Set<Integer> set1, Set<Integer> set2) {
+        return set2.containsAll(set1);
+    }
+
+    public static void main(String[] args) {
+        Set<Integer> set1 = new HashSet<>();
+        set1.add(2);
+        set1.add(3);
+
+        Set<Integer> set2 = new HashSet<>();
+        set2.add(1);
+        set2.add(2);
+        set2.add(3);
+        set2.add(4);
+
+        System.out.println(isSubset(set1, set2)); // Output: true
+    }
+}
