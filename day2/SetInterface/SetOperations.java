24d6e230c9156d6e6bc7ca10bfd06f61bbb1eb8e
unknown <kapilnamdeo2003@gmail>
2025-02-12 12:04:12 +0530

    [Kapil Namdeo]add. Set Operations program

---
+++ day2/SetInterface/SetOperations.java
@@ -0,0 +1,34 @@
+package com.capgeminiTrainingPrograms.week4.day2.SetInterface;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetOperations {
+    public static void main(String[] args) {
+        Set<Integer> set1 = new HashSet<>();
+        set1.add(1);
+        set1.add(2);
+        set1.add(3);
+
+        Set<Integer> set2 = new HashSet<>();
+        set2.add(3);
+        set2.add(4);
+        set2.add(5);
+
+        System.out.println("Union: " + getUnion(set1, set2));
+        System.out.println("Intersection: " + getIntersection(set1, set2));
+    }
+
+    public static Set<Integer> getUnion(Set<Integer> set1, Set<Integer> set2) {
+        Set<Integer> unionSet = new HashSet<>(set1);
+        unionSet.addAll(set2);
+        return unionSet;
+    }
+
+    public static Set<Integer> getIntersection(Set<Integer> set1, Set<Integer> set2) {
+        Set<Integer> intersectionSet = new HashSet<>(set1);
+        intersectionSet.retainAll(set2);
+        return intersectionSet;
+    }
+}
+
