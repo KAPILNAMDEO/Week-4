4f8f990a8756bd9f3e0c21084920ab4f74451847
unknown <kapilnamdeo2003@gmail>
2025-02-14 15:36:36 +0530

    [Kapil Namdeo ]add. Bank Transaction System program

---
+++ day4/BankTransactionSystem.java
@@ -0,0 +1,48 @@
+package com.capgeminiTrainingPrograms.week4.day4;
+
+
+
+class InsufficientBalanceException extends Exception {
+    public InsufficientBalanceException(String message) {
+        super(message);
+    }
+}
+
+class BankAccount {
+    private double balance;
+
+    public BankAccount(double initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    public void withdraw(double amount) throws InsufficientBalanceException {
+        if (amount < 0) {
+            throw new IllegalArgumentException("Invalid amount!");
+        }
+        if (amount > balance) {
+            throw new InsufficientBalanceException("Insufficient balance!");
+        }
+        balance -= amount;
+        System.out.println("Withdrawal successful, new balance: " + balance);
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
+
+public class BankTransactionSystem {
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount(300);
+
+        try {
+            account.withdraw(500);
+            account.withdraw(600);
+        } catch (InsufficientBalanceException e) {
+            System.out.println(e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
