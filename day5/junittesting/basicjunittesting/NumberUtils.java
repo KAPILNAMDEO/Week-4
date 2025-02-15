c8715ffdb1080271f6a357e68600ad4a26f8b819
unknown <kapilnamdeo2003@gmail>
2025-02-15 23:02:23 +0530

    [Kapil Namdeo]add.NumberUtils java program

---
+++ day5/junittesting/basicjunittesting/NumberUtils.java
@@ -0,0 +1,10 @@
+package com.capgeminiTrainingPrograms.week4.day5.junittesting.basicjunittesting;
+
+
+
+public class NumberUtils {
+    // Method to check if a number is even
+    public static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+}
