faaed1dd2d2981149993b5eecd8a864aee92084d
unknown <kapilnamdeo2003@gmail>
2025-02-17 00:25:42 +0530

    [kapil Namdeo]add.BugReport Processor Test Program

---
+++ day6ReflectionandannotationsTest/annotations/exercise/BugReportProcessorTest.java
@@ -0,0 +1,20 @@
+package com.capgeminiTrainingPrograms.week4.day6Reflectionandannotations.annotations.exercise;
+
+import org.junit.jupiter.api.Test;
+import java.lang.reflect.Method;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BugReportTest {
+    @Test
+    void testBugReportAnnotation() throws NoSuchMethodException {
+        Method method = SoftwareModule.class.getMethod("processData");
+
+        assertTrue(method.isAnnotationPresent(BugReports.class));
+
+        BugReports bugReports = method.getAnnotation(BugReports.class);
+        assertEquals(2, bugReports.value().length);
+
+        assertEquals("NullPointerException occurs when input is null.", bugReports.value()[0].description());
+        assertEquals("Performance issue when processing large data sets.", bugReports.value()[1].description());
+    }
+}
